dd6f77e45dc9ddebb77cc9878daffea14af201f1
Trend20 <enockomondi305@gmail.com>
2022-12-01 17:43:27 +0300

    types and reference

--- Reference.js
+++ Reference.js
@@ -11,3 +11,21 @@ var myNum = 20;
 // GOOD
 const name = 'mike';
 const myNum = 20;
+
+// If you must reassign the variable then use let and not var.
+// why: let is block scoped as opposed to var which is function scoped
+
+let name = 'mike';
+let age = 24;
+
+// bad
+var count = 1;
+if (true) {
+  count += 1;
+}
+
+// good, use the let.
+let count = 1;
+if (true) {
+  count += 1;
+}
